47456bd791ffc73e054976e2d2ea4fc51e133f5a
Alok Kumar Mishra <alok99893@gmail.com>
2023-12-27 21:03:06 +0530

    final touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import {TodoProvider} from "./contexts"
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
@@ -35,7 +34,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
-    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="bg-[#3e2a74] min-h-screen py-8">
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
         <div className="mb-4">
